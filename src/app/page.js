2343fbd6e9473c999fc83c161bb56b2abf29a1ed
Shivam <shivampandey88@hotmail.com>
2024-03-30 14:50:02 +0530

    Updated for vercel deployment

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="temp-title">Home page coming soon. Till then click on "Join us" for login/signup</h1>
+    <h1 className="temp-title">
+      Home page coming soon. Till then click on Join us for login/signup
+    </h1>
   );
 }
