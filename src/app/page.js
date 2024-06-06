8664334b1f9ce67f49235fd86918dce07f573d54
Shivam Pandey <shivampandey88@hotmail.com>
2024-06-06 16:15:40 +0530

    Updated home page

--- src/app/page.js
+++ src/app/page.js
@@ -26,14 +26,20 @@ export default function Home() {
           <img src="/Images/map.png" alt="map" />
         </div>
       </div>
-      {/* <div>
-        <Image width={350} height={70} src="/Images/boat.png" alt="about" />
-        <Image width={350} height={70} src="/Images/camp.png" alt="about" />
+      <div className="relative mb-10 md:mb-60 flex justify-center items-center">
+        <div className="w-[90vw] md:w-[80vw]">
+          <img src="/Images/boat.png" alt="boat" />
+        </div>
+        <div className="hidden absolute top-60 mt-20 left-20 w-[90vw] md:w-[30vw] md:block">
+          <img src="/Images/camp.png" alt="camp" />
+        </div>
       </div>
-      <div className="flex">
+      <div className="flex flex-col md:flex-row justify-center items-center mt-14">
         <div>
-          <h1>Connect with fellow travelers </h1>
-          <p>
+          <h1 className="text-2xl  my-4 md:text-4xl text-center md:text-start">
+            Connect with fellow travelers{" "}
+          </h1>
+          <p className="w-[90vw] mb-2 mx-auto  md:w-[40vw] md:mx-0">
             With Wandermate, you can easily book a variety of thrilling tours
             and treks that cater to your adventurous spirit. But we dont stop
             there. We understand that the best journeys are shared, which is why
@@ -42,12 +48,19 @@ export default function Home() {
             activities, and build lasting friendships that extend beyond the
             trip.
           </p>
+          <p className="w-[90vw] mb-2 mx-auto  md:w-[40vw] md:mx-0">
+            Our community-driven approach ensures that you are never alone on
+            your journey. Share tips, plan activities, and get to know your
+            travel companions before you even set foot on your adventure. With
+            wandermate, you are not just booking a trip; you are joining a
+            community.
+          </p>
         </div>
-        <div>
-          <Image width={350} height={70} src="/Images/about.jpg" alt="about" />
+        <div className="w-[90vw] md:w-[35vw] md:ml-12 ">
+          <img src="/Images/Group.jpeg" alt="group" />
         </div>
       </div>
-      <div>
+      {/*  <div>
         <div>
           <h1>Our features</h1>
           <Image width={350} height={70} src="/Images/about.jpg" alt="about" />
